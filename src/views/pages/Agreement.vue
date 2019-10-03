4bb8c945c554f836a3b3ed10960c70bf74854474
MadfishDT <lafefish@gmail.com>
2019-10-03 20:33:34 +0900

    agreement korea

--- src/views/pages/Agreement.vue
+++ src/views/pages/Agreement.vue
@@ -4,7 +4,7 @@
         <div class="container">
 
             <b-row class="justify-content-center w-90 mt-5 pb-0">
-                <h1>Agreement</h1>
+                <h1>약관 동의</h1>
             </b-row>
             <b-row class="justify-content-center w-90 h-100 mt-5 d-flex">
                 <b-card class="col-md-8 h-100">
@@ -19,17 +19,17 @@
                     <b-row align-h="end" class="mt-3">
                         <b-col cols="5">
                             <div class="col-12 input-group">
-                                <b-radio v-model="select" key="Agree" name="agree" value="agree" class="mb-3 ml-3 radio-inline">Agree</b-radio>
-                                <b-radio v-model="select" key="Reject" name="reject" value="reject" class="mb-3 ml-3 radio-inline">Reject</b-radio>
+                                <b-radio v-model="select" key="Agree" name="agree" value="agree" class="mb-3 ml-3 radio-inline">동의</b-radio>
+                                <b-radio v-model="select" key="Reject" name="reject" value="reject" class="mb-3 ml-3 radio-inline">거부</b-radio>
                             </div>
                         </b-col>
                     </b-row>
                     <b-row align-h="end" class="mt-3">
                          <b-col cols="3">
-                             <b-button block @click="back" class="px-0">Back</b-button>
+                             <b-button block @click="back" class="px-0">돌아가기</b-button>
                         </b-col>
                         <b-col cols="3">
-                             <b-button block @click="submit" class="px-0">Submit</b-button>
+                             <b-button block @click="submit" class="px-0">등록</b-button>
                         </b-col>
                     </b-row>
                 </b-card>
